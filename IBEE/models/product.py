c1fdaa3c3f872250e7ef5ce1363717ab82defd5d
Marc Tormo i Bochaca <batista10.info@gmail.com>
2022-07-12 11:34:00 +0200

    IBEE

---
+++ IBEE/models/product.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class product(models.Model):
+    _inherit = 'product.template'
+
+    IBEE = fields.Selection((('0','Exempt'),('0.1','0.1'),('0.15','0.15')),'IBEE', default='Exempt')
+    litres_IBEE = fields.Float('Liters IBEE', default=0)
+
+
+
