95ae9902e57ddbb596f565c68a9291cd89449cd3
Batista10 - Marc Tormo i Bochaca <batista10.info@gmail.com>
2022-07-14 11:15:11 +0200

    Creació Mòdul IBEE Odoo 12

---
+++ IBEE/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 - Marc Tormo i Bochaca - Batista10
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'IBEE',
+    'summary': 'IBEE - Impost Begudes Ensucrades Envasades',
+    'author': 'Marc Tormo - Batista10',
+    'website': "https://github.com/B10Serveis/Odoo-addons/IBEE",
+    'depends': ['account', 'product', 'sale'],
+    'version': '12.0.0.1.0',
+    'license': 'AGPL-3', 
+    'application': True,
+    'category': 'Account',
+    'data': [
+            'views/product_IBEE.xml',
+            'views/sale_IBEE.xml',
+            'views/invoice_IBEE.xml',
+    ],
+}
--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Projectes de mòduls de Odoo de Batista10.
 
 Projectes:
 
-* Hide Cost: Amaga costos per usuaris
-* Pos Closing Report: Informe de tancament de caixes TPV
-* Product Margin Classification: Estableix preu de venta en funció de costos
+* Hide Cost: Amaga costos per usuaris   
+* Pos Closing Report: Informe de tancament de caixes TPV   
+* Product Margin Classification: Estableix preu de venta en funció de costos   
+* IBEE: Gestió del Impost de Begudes Ensucrades   
