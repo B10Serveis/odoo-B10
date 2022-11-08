f594d4cb3e15cbdec506b6358fe9c6794400ba4b
Batista10-G <gestio@batista10.com>
2022-11-08 10:00:51 +0100

    state_original_name a v 15.0

--- state_original_name/__manifest__.py
+++ state_original_name/__manifest__.py
@@ -4,7 +4,7 @@
     'author': "Batista10",
     'website': "https://www.batista10.cat",
     'category': 'Personalization',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'depends': ['base', 'contacts'],
     'license': 'AGPL-3',
     'application': True,
