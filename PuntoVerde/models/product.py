f7732972f0b23d17235026280e9aa90160ea611b
Batista10-G <gestio@batista10.com>
2023-01-23 13:13:58 +0100

    PuntoVerde - Textos

--- PuntoVerde/models/product.py
+++ PuntoVerde/models/product.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class product(models.Model):
     _inherit = 'product.template'
 
-    PuntoVerde = fields.Float('PuntoVerde', default=0, digits=(12, 3))
+    PuntoVerde = fields.Float('Punt Verd', default=0, digits=(12, 3))
