0f42e2384f7db40851dfb800186e04bb7d40981c
Batista10-G <gestio@batista10.com>
2023-04-05 16:08:14 +0200

    IBEE: punt verd a 0 si descompte = 100

--- IBEE/models/sales.py
+++ IBEE/models/sales.py
@@ -6,20 +6,27 @@ from odoo.tools.misc import formatLang
 class sale_order_line_IBEE(models.Model):
     _inherit = 'sale.order.line'
 
-    PuntoVerde_sale = fields.Float(string='PuntoVerde', compute='_compute_amount')
+    PuntoVerde_sale = fields.Float(
+        string='PuntoVerde', compute='_compute_amount')
     IBEE_sale = fields.Float(string='IBEE', compute='_compute_amount')
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
-       
+
         for line in self:
-            IBEE_unit=line.product_id.litres_IBEE * float(line.product_id.IBEE)
-            PuntoVerde_unit=float(line.product_id.PuntoVerde)
-            
+            IBEE_unit = line.product_id.litres_IBEE * \
+                float(line.product_id.IBEE)
+
+            if line.discount == 100:
+                PuntoVerde_unit = 0
+            else:
+                PuntoVerde_unit = float(line.product_id.PuntoVerde)
+
             Line_PuntoVerde = PuntoVerde_unit * line.product_uom_qty
             Line_IBEE = IBEE_unit * line.product_uom_qty
-            
-            price = (line.price_unit * (1 - (line.discount or 0.0) / 100.0)) + PuntoVerde_unit + IBEE_unit
+
+            price = (line.price_unit * (1 - (line.discount or 0.0) /
+                     100.0)) + PuntoVerde_unit + IBEE_unit
 
             taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
                                             product=line.product_id, partner=line.order_id.partner_shipping_id)
@@ -40,7 +47,8 @@ class sale_order_line_IBEE(models.Model):
         """
         self.ensure_one()
         res = {}
-        product = self.product_id.with_context(force_company=self.company_id.id)
+        product = self.product_id.with_context(
+            force_company=self.company_id.id)
         account = product.property_account_income_id or product.categ_id.property_account_income_categ_id
 
         if not account and self.product_id:
@@ -69,22 +77,31 @@ class sale_order_line_IBEE(models.Model):
             'PuntoVerde_invoice': self.PuntoVerde_sale,
         }
         return res
-    
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    
+
     @api.multi
     def _amount_by_group(self):
         for order in self:
             currency = order.currency_id or order.company_id.currency_id
-            fmt = partial(formatLang, self.with_context(lang=order.partner_id.lang).env, currency_obj=currency)
+            fmt = partial(formatLang, self.with_context(
+                lang=order.partner_id.lang).env, currency_obj=currency)
             res = {}
             for line in order.order_line:
-                IBEE_unit=line.product_id.litres_IBEE * float(line.product_id.IBEE)
-                PuntoVerde_unit=float(line.product_id.PuntoVerde)
-                
-                price_reduce = (line.price_unit * (1 - (line.discount or 0.0) / 100.0)) + PuntoVerde_unit + IBEE_unit
-                taxes = line.tax_id.compute_all(price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)['taxes']
+                IBEE_unit = line.product_id.litres_IBEE * \
+                    float(line.product_id.IBEE)
+                    
+                if line.discount == 100:
+                    PuntoVerde_unit = 0
+                else:
+                    PuntoVerde_unit = float(line.product_id.PuntoVerde)
+
+                price_reduce = (
+                    line.price_unit * (1 - (line.discount or 0.0) / 100.0)) + PuntoVerde_unit + IBEE_unit
+                taxes = line.tax_id.compute_all(
+                    price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)['taxes']
                 for tax in line.tax_id:
                     group = tax.tax_group_id
                     res.setdefault(group, {'amount': 0.0, 'base': 0.0})
@@ -98,7 +115,7 @@ class SaleOrder(models.Model):
                 fmt(l[1]['amount']), fmt(l[1]['base']),
                 len(res),
             ) for l in res]
-   
+
     @api.multi
     def action_invoice_create(self, grouped=False, final=False):
         invoice_ids = super(SaleOrder, self).action_invoice_create(
@@ -108,12 +125,13 @@ class SaleOrder(models.Model):
         for inv in invoices:
             inv._onchange_invoice_line_ids()
         return invoice_ids
-    
+
 
 class account_invoice_line_IBEE(models.Model):
     _inherit = 'account.invoice.line'
 
-    PuntoVerde_invoice = fields.Float(string='PuntoVerde', compute='_compute_price')
+    PuntoVerde_invoice = fields.Float(
+        string='PuntoVerde', compute='_compute_price')
     IBEE_invoice = fields.Float(string='IBEE', compute='_compute_price')
 
     @api.one
@@ -121,18 +139,24 @@ class account_invoice_line_IBEE(models.Model):
                  'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
                  'invoice_id.date_invoice', 'invoice_id.date')
     def _compute_price(self):
-        IBEE_unit=self.product_id.litres_IBEE * float(self.product_id.IBEE)
-        PuntoVerde_unit=float(self.product_id.PuntoVerde)
+        IBEE_unit = self.product_id.litres_IBEE * float(self.product_id.IBEE)
         
+        if self.discount == 100:
+            PuntoVerde_unit = 0
+        else:    
+            PuntoVerde_unit = float(self.product_id.PuntoVerde)
+
         self.PuntoVerde_invoice = PuntoVerde_unit * self.quantity
         self.IBEE_invoice = IBEE_unit * self.quantity
 
         currency = self.invoice_id and self.invoice_id.currency_id or None
-        price = (self.price_unit * (1 - (self.discount or 0.0) / 100.0)) + PuntoVerde_unit + IBEE_unit
+        price = (self.price_unit * (1 - (self.discount or 0.0) / 100.0) ) + PuntoVerde_unit + IBEE_unit
         taxes = False
         if self.invoice_line_tax_ids:
-            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
-        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
+            taxes = self.invoice_line_tax_ids.compute_all(
+                price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
+        self.price_subtotal = price_subtotal_signed = taxes[
+            'total_excluded'] if taxes else self.quantity * price
         self.price_total = taxes['total_included'] if taxes else self.price_subtotal
         if self.invoice_id.currency_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
             currency = self.invoice_id.currency_id
@@ -142,24 +166,32 @@ class account_invoice_line_IBEE(models.Model):
         sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
         self.price_subtotal_signed = price_subtotal_signed * sign
 
+
 class account_invoice_IBEE(models.Model):
     _inherit = 'account.invoice'
-            
+
     @api.multi
     def get_taxes_values(self):
-        
+
         tax_grouped = {}
         round_curr = self.currency_id.round
         for line in self.invoice_line_ids:
-            IBEE_unit=line.product_id.litres_IBEE * float(line.product_id.IBEE)
-            PuntoVerde_unit=float(line.product_id.PuntoVerde)
+            IBEE_unit = line.product_id.litres_IBEE * \
+                float(line.product_id.IBEE)
+            if line.discount == 100:
+                PuntoVerde_unit = 0
+            else:
+                PuntoVerde_unit = float(line.product_id.PuntoVerde)
             if not line.account_id or line.display_type:
                 continue
-            price_unit = (line.price_unit * (1 - (line.discount or 0.0) / 100.0)) + PuntoVerde_unit + IBEE_unit
-            taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
+            price_unit = (line.price_unit * (1 - (line.discount or 0.0) /
+                          100.0)) + PuntoVerde_unit + IBEE_unit
+            taxes = line.invoice_line_tax_ids.compute_all(
+                price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
             for tax in taxes:
                 val = self._prepare_tax_line_vals(line, tax)
-                key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
+                key = self.env['account.tax'].browse(
+                    tax['id']).get_grouping_key(val)
 
                 if key not in tax_grouped:
                     tax_grouped[key] = val
