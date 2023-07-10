374bc3157cf977c0d7ae69fcd6cae2cfa08afc9d
Batista10-G <gestio@batista10.com>
2023-07-10 10:52:35 +0200

    product_margin_classification: 13.0

---
+++ product_margin_classification/models/product_product.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2018-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    # Define a variant function to allow inherited modules to call
+    # use_theoritical_price for product variants
+    
+    def use_theoretical_price_variant(self):
+        self.mapped('product_tmpl_id').use_theoretical_price()
