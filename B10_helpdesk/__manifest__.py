b412ad67cc29aa5b5f8d796896ad0e23a93785ef
Batista10-G <gestio@batista10.com>
2023-09-15 10:54:01 +0200

    B10_helpdesk a v16.0

---
+++ B10_helpdesk/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Batista10 - Helpdesk",
+    'summary': """Personalització Centre d'Assistència Batista10""",   
+    'author': "Batista10",
+    'website': "https://www.batista10.cat",
+    'category': 'Personalization',
+    'version': '16.0.1.1.0',
+    'depends': ['helpdesk_mgmt','contract'],
+    'license': 'AGPL-3', 
+    'application': True,
+    'data': [
+			'views/helpdeskB10.xml',
+			],
+}
---
+++ B10_helpdesk/models/helpdeskB10.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+class helpdeksB10(models.Model):
+    _inherit = 'helpdesk.ticket'
+    internal_note = fields.Html(string="Internal Note", required=False, )
+    close_note = fields.Html(string="Closing Note", required=False, )
+    email_ccs = fields.Many2many('res.partner', 'ticket_partner_rel', 'ticket_ids', 'partner_id',  string="CC", domain=[('email','=like','%@%')])
+    related_contract = fields.Many2one('contract.contract', string="Contract")
+
+class partner_helpdesk_B10(models.Model):
+        _inherit = 'res.partner'
+        cc_email = fields.Many2many('helpdesk.ticket', 'ticket_partner_rel','partner_id','ticket_ids')
