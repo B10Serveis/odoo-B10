65a39f6129b4f4efa5cca30576c217f026e65f22
batista10 <batista10@pc-marc>
2023-12-06 09:34:55 +0100

    Heldesk warning hours

--- B10_helpdesk/models/helpdeskB10.py
+++ B10_helpdesk/models/helpdeskB10.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields, _
+import logging
 
 class helpdeksB10(models.Model):
     _inherit = 'helpdesk.ticket'
@@ -8,7 +9,15 @@ class helpdeksB10(models.Model):
     related_contract = fields.Many2one('contract.contract', string="Contract")
     worked_hours = fields.Float(string="Worked Hours", required='False')
     stage_closed = fields.Boolean(string='Stage closed', related='stage_id.closed')
+    
+    @api.onchange('close_note')
+    def on_change_state(self):
+        for rec in self:
+            if rec.worked_hours == 0 and rec.stage_closed == 1:
+                return {'warning': {'title': "Empty Worked Hours", 'message': "Value from Wroked Hours can't be 0", 'type': 'notification'},}
 
 class partner_helpdesk_B10(models.Model):
         _inherit = 'res.partner'
         cc_email = fields.Many2many('helpdesk.ticket', 'ticket_partner_rel','partner_id','ticket_ids')
+        
+
