5a85fce62b1fd369f50371bbc4dc9a4f97cdcd7b
Batista10-G <gestio@batista10.com>
2024-01-23 10:09:41 +0100

    acc_number_several_partners: 14.0

--- acc_number_serveral_partners/models/models.py
+++ acc_number_serveral_partners/models/models.py
@@ -3,8 +3,11 @@ from odoo import models, fields
 
 class ResPartnerBank_inherit(models.Model):
     _inherit = "res.partner.bank"
-    
+
     _sql_constraints = [
-        ('unique_number', 'unique(sanitized_acc_number, company_id, partner_id)',
-         'Account Number must be unique for Partner'),
+        (
+            "unique_number",
+            "unique(sanitized_acc_number, company_id, partner_id)",
+            "Account Number must be unique for Partner",
+        ),
     ]
