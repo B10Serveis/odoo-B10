75c3124a07105f12e92a7a91a2cb58eca6cbe84e
Batista10-G <gestio@batista10.com>
2024-01-23 13:08:53 +0100

    pos_closing_report: manifest i readme 14.0

--- pos_closing_report/README.rst
+++ pos_closing_report/README.rst
@@ -8,3 +8,30 @@ Registre de Versions:
 - 14.0.0.1.0
 
 * Traspàs des de versió 13
+
+Authors
+~~~~~~~
+
+* Batista10
+
+
+Contributors
+~~~~~~~~~~~~
+
+* Marc Tormo i Bochaca <marc@batista10.cat> (https://www.batista10.cat)
+* Joan Llimiñana i Sabaté <joan@batista10.cat> (https://www.batista10.cat)
+
+
+Other credits
+~~~~~~~~~~~~~
+
+The development of this module has been financially supported by:
+
+* Batista10
+
+
+Maintainers
+~~~~~~~~~~~
+
+This module is maintained by batista10.
+
--- pos_closing_report/__manifest__.py
+++ pos_closing_report/__manifest__.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Informe de tancament de caixa TPV",
-    'summary': """Informe de tancament de caixa TPV""",   
-    'author': "Batista10",
-    'website': "https://www.batista10.cat",
-    'category': 'Reports',
-    'version': '14.0.0.1.0',
-    'depends': ['base','point_of_sale'],
-    'license': 'AGPL-3', 
-    'application': True,
-    'data': [
-			'wizard/pos_wizard_report.xml',
-        	'views/pos_closing.xml',
-        	'views/pos_closing_report.xml'
-			],
-	'installable': False,
+    "name": "Informe de tancament de caixa TPV",
+    "summary": """Informe de tancament de caixa TPV""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Reports",
+    "version": "14.0.0.1.0",
+    "depends": ["base", "point_of_sale"],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [
+        "wizard/pos_wizard_report.xml",
+        "views/pos_closing.xml",
+        "views/pos_closing_report.xml",
+    ],
+    "installable": False,
 }
-
