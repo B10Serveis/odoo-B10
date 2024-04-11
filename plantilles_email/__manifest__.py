606d8e25dae3c0cb9f0f9d2260b3ea5833617656
Batista10-G <gestio@batista10.com>
2024-04-11 18:04:28 +0200

    plantilles_email: traduccions i num versió

--- plantilles_email/__manifest__.py
+++ plantilles_email/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "15.0.1.1.1",
+    "version": "16.0.1.1.1",
     "depends": [
         "base",
         "mail",
