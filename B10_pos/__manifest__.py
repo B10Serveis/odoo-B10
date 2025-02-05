94e56709c492f8a9bab0f9450a51c90d505b3438
Batista10-G <gestio@batista10.com>
2025-02-05 17:35:14 +0100

    B10_pos: creat a v17.0

---
+++ B10_pos/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Batista10 - POS",
+    "summary": """Personalització POS Batista10""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Personalization",
+    "version": "17.0.1.0.0",
+    "depends": ["point_of_sale"],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [],
+}
---
+++ B10_pos/readme/HISTORY.rst
@@ -0,0 +1,5 @@
+17.0.1.0.0 (2025-02-05)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* [ADD] Creat a V.17.
+* [ADD] Modifica product.product perque el POS vegi les existencies per Ubicació enlloc de per Magatzem.
