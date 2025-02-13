a85d6a1df6bc8d2233316f00b23461467c41dd9d
Batista10-G <gestio@batista10.com>
2025-02-13 16:49:48 +0100

    B10_account: signa el 'Banc Receptor' automàticament si en una factura assignem una forma de pagament que te un diari bancari fix

---
+++ B10_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Batista10 - Account",
+    "summary": """Personalitzacions de Facturació""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Personalization",
+    "version": "15.0.1.0.0",
+    "depends": ["account"],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [],
+}
---
+++ B10_account/readme/HISTORY.rst
@@ -0,0 +1,6 @@
+16.0.1.0.0 (2025-02-11)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* [ADD] Creat a V.16.
+* [ADD] Assigna el 'Banc Receptor' automàticament si en una factura assignem una forma de pagament que te un diari bancari fix.
+
