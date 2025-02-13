91ee5114c9b0745dd352cec7437b9e2046485ebd
Batista10-G <gestio@batista10.com>
2025-02-13 16:24:34 +0100

    B10_account: Assigna el 'Banc Receptor' automàticament si en una factura assignem una forma de pagament que te un diari bancari fix.

---
+++ B10_account/models/B10_account_move.py
@@ -0,0 +1,30 @@
+from odoo import api, models, fields, _
+
+
+class B10AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.onchange("payment_mode_id")
+    def _onchange_payment_mode_id_b10(self):
+        for move in self:
+            # Que fem si la factura ja te ficat un num de compte i canviem el mètode de pagament?
+            if (
+                move.payment_mode_id.payment_method_id.code == "bank_transfer"
+                and move.payment_mode_id.bank_account_link == "fixed"
+                and move.payment_mode_id.fixed_journal_id
+            ):
+                if move.payment_mode_id.fixed_journal_id.bank_account_id.acc_number:
+                    move.partner_bank_id = (
+                        move.payment_mode_id.fixed_journal_id.bank_account_id
+                    )
+
+    @api.depends("bank_partner_id")
+    def _compute_partner_bank_id(self):
+        for move in self:
+            # This will get the bank account from the partner in an order with the trusted first
+            bank_ids = move.bank_partner_id.bank_ids.filtered(
+                lambda bank: not bank.company_id or bank.company_id == move.company_id
+            ).sorted(lambda bank: not bank.allow_out_payment)
+            # B10: Afegim condicional perque no reescrigui si ja n'hi ha
+            if not move.partner_bank_id:
+                move.partner_bank_id = bank_ids[:1]
