d644dd203e367c4b8ed117586c3e90c5a2903d4d
Batista10-G <gestio@batista10.com>
2025-07-04 15:42:22 +0200

    field_encryption

---
+++ field_encryption/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Field Encryption",
+    "summary": """Encrypted fields data in database.""",
+    "category": "Technical Settings",
+    "version": "16.0.0.1",
+    "license": "AGPL-3",
+    "author": "Shah Alam Sumon",
+    "website": "https://github.com/ShahAlamSumon/field_encryption",
+    "images": ["static/description/banner.gif"],
+    "depends": ["base"],
+    "external_dependencies": {
+        "python": ["cryptography"],
+    },
+}
