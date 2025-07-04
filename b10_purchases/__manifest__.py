02d9e006f66ac04bfd260ae5f95df520b5ea1f96
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-04 13:51:16 +0200

    B10_purchases: afegit checkbox “Accés Proveïdor” per mostrar la pestanya “Access” a res.partner amb els camps URL, usuari i contrasenya encriptada.

---
+++ b10_purchases/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Batista 10 - Purchases",
+    "summary": """Personalització plataformes externes de proveïdors""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Personalization",
+    "version": "15.0.1.5.3",
+    "depends": [
+        "contacts",
+        "purchase",
+        "l10n_es_partner",
+        "l10n_es_partner_mercantil",
+        "field_encryption",
+    ],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [
+        "views/B10_supplier_platform.xml",
+    ],
+}
