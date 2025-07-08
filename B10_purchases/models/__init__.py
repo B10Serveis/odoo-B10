39f0b5c99fdc0e214fc448a39ddc1b247125c1df
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-08 09:51:17 +0200

    B10_purchases --> fix(res_groups): corregit error que permetia usuaris estar simultàniament en els grups manager i reader, eliminant automàticament el grup contrari

--- B10_purchases/models/__init__.py
+++ B10_purchases/models/__init__.py
@@ -31,3 +31,5 @@
 #
 ###############################################
 from . import supplierPlatformb10
+from . import res_users  
+from . import res_groups
---
+++ B10_purchases/models/res_groups.py
@@ -0,0 +1,38 @@
+from odoo import api, models
+
+# Estén el model 'res.groups' per afegir lògica personalitzada per a les modificacions de grups.
+class ResGroups(models.Model):
+    _inherit = 'res.groups'
+
+    def write(self, vals):
+        res = super().write(vals)
+        GroupMgr  = self.env.ref('B10_purchases.group_b10_pwd_manager')
+        GroupRead = self.env.ref('B10_purchases.group_b10_pwd_reader')
+
+        # Només ens interessa si modifiquen la many2many 'users'
+        if 'users' in vals:
+            # Extraiem els IDs d'usuaris que s'estan AFEGINT
+            add_ids = []
+            for cmd in vals['users']:
+                if cmd[0] == 4:      # (4, user_id, 0)
+                    add_ids.append(cmd[1])
+                elif cmd[0] == 6:    # (6, 0, [ids])
+                    add_ids += cmd[2]
+
+            # Si estem escrivint sobre el grup Manager, traiem aquests usuaris del Reader
+            if self.filtered(lambda g: g.id == GroupMgr.id) and add_ids:
+                conflicte = GroupRead.users.filtered(lambda u: u.id in add_ids)
+                if conflicte:
+                    GroupRead.sudo().write({
+                        'users': [(3, u.id) for u in conflicte]
+                    })
+
+            # Si estem escrivint sobre el grup Reader, traiem aquests usuaris del Manager
+            if self.filtered(lambda g: g.id == GroupRead.id) and add_ids:
+                conflicte = GroupMgr.users.filtered(lambda u: u.id in add_ids)
+                if conflicte:
+                    GroupMgr.sudo().write({
+                        'users': [(3, u.id) for u in conflicte]
+                    })
+
+        return res
