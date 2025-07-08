74fd17dc3db7ff30f07d974ca7eb828bab31c533
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-08 11:31:41 +0200

    B10_sales: nou mòdul amb el filtre "Caducats" a la vista de recerca de pressupostos

---
+++ B10_sales/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "B10 - Sales",
+    "summary": """B10 Sales""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Personalization",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "sale",
+    ],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [
+        "views/B10_sale_order_expired_filter.xml",
+    ],
+}
