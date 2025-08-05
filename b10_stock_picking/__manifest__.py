1d8525ce09bbb7549d1018de3f512768ecba5da8
PolComas <pcomasro93@alumnes.ub.edu>
2025-08-05 12:55:38 +0200

    b10_stock_picking: afegeix informe “Entrega Estandard” per a notes de lliurament amb capçalera B10, lots i footer GDPR

---
+++ b10_stock_picking/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "B10 - Stock Picking",
+    "summary": "Personalització de la impressió de les entregues",
+    "version": "18.0.1.0.0",
+    "category": "Warehouse/Reporting",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "report/entrega_batista.xml",
+        "report/report_entrega_batista.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
