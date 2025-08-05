849a5119857cba684618c1f9102842ea9c7e648c
PolComas <pcomasro93@alumnes.ub.edu>
2025-08-05 08:25:26 +0200

    b10_contacts: Trasllat de State Original Name a b10_contactes, nou camp `original_name` a `res.country.state` amb càrrega de noms d’estats espanyols via CSV

---
+++ b10_contacts/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "B10 Contactes",
+    "summary": "Personalitzacions de contactes",
+    "version": "18.0.1.0.0",
+    "category": "Personalization",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "license": "AGPL-3",
+    "depends": ["base", "contacts"],
+    "data": [
+        "data/res.country.state.csv",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
---
+++ b10_contacts/models/state_original_name.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+class StateOriginalName(models.Model):
+    _inherit = 'res.country.state'
+    original_name = fields.Char("Original name", required=False)
