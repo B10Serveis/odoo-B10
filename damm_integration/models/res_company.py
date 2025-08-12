135054cca510b674ff0bef80014e31f5e956509a
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-08-12 12:53:17 +0200

    damm_integration: Nou camp de codi de distribuïdor Damm.
    Editable des de la vista de companyia i la de configuració.

--- damm_integration/models/res_company.py
+++ damm_integration/models/res_company.py
@@ -7,6 +7,9 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    damm_dealer_code = fields.Char(string="Damm dealer code",
+                                   help=("The code used by Damm to identify your company"
+                                         " as a dealer of its products"))
     damm_partner_id = fields.Many2one("res.partner", string="Damm company",
                                       help=("Used to locate products and customers"
                                             " when generating reports for Damm"),
--- damm_integration/models/res_config_settings.py
+++ damm_integration/models/res_config_settings.py
@@ -10,6 +10,9 @@ class ResConfigSettings(models.TransientModel):
     group_damm_integration = fields.Boolean(
         string="Damm integration",
         implied_group="damm_integration.res_groups_damm_integration")
+    damm_dealer_code = fields.Char(
+        related="company_id.damm_dealer_code", readonly=False,
+        groups="damm_integration.res_groups_damm_integration")
     damm_partner_id = fields.Many2one(
         related="company_id.damm_partner_id", readonly=False,
         groups="damm_integration.res_groups_damm_integration")
