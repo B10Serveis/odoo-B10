a5c7d7e8f0fb9e36e26800c8af6b1d8c75a80df7
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-08-12 11:31:00 +0200

    damm_integration: Vista per a configuració de mòdul.

--- __manifest__.py
+++ __manifest__.py
@@ -21,8 +21,10 @@ En particular, permet generar els següents informes en el format necessari per 
     * Dades de les condicions comercials dels clients
     * Vendes als clients
 """,
+    "depends": ["base_setup"],
     "data": [
         "security/groups.xml",
         "views/res_company_view.xml",
+        "views/res_config_settings_view.xml",
     ],
 }
