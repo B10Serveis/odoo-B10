6fb50a2a1f67b30a7773369a15f97ba70b77b62c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-09-04 11:21:28 +0200

    damm_integration: Mou menú Damm a informes de facturació.

--- __manifest__.py
+++ __manifest__.py
@@ -25,12 +25,11 @@ En particular, permet generar els següents informes en el format necessari per 
         "base_setup",  # res.config.settings view
         "base_vat",  # res.partner.vat
         "l10n_es_partner",  # res.partner.comercial
-        "account",  # account.move
+        "account",  # account.move, reports menu
     ],
     "data": [
         "security/groups.xml",
         "security/ir.model.access.csv",
-        "views/menus.xml",
         "views/res_company_view.xml",
         "views/res_config_settings_view.xml",
         "wizards/reports_view.xml",
