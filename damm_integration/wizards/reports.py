68c0c950efe3519cb68076c75c25710d8c1a0eb8
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-09-10 16:19:52 +0200

    damm_integration: Missatges d'excepció marcats com a traduïbles.

--- damm_integration/wizards/reports.py
+++ damm_integration/wizards/reports.py
@@ -5,7 +5,7 @@ import csv
 import io
 import itertools
 
-from odoo import api, exceptions, models, fields
+from odoo import _, api, exceptions, models, fields
 
 
 # Report item field getters and value formatters.
@@ -199,7 +199,7 @@ class DammReportsWizard(models.TransientModel):
         for report in self:
             if report.date_start > report.date_end:
                 raise models.ValidationError(
-                    "Report end date must be greater than its start date.")
+                    _("Report end date must be greater than its start date."))
 
     @api.model
     def create(self, values):
@@ -207,11 +207,11 @@ class DammReportsWizard(models.TransientModel):
         dealer_code = company.damm_dealer_code
         if not dealer_code:
             raise exceptions.UserError(
-                "Please configure your company's Damm dealer code.")
+                _("Please configure your company's Damm dealer code."))
         damm_partner = company.damm_partner_id
         if not damm_partner:
             raise exceptions.UserError(
-                "Please configure which partner is the Damm company.")
+                _("Please configure which partner is the Damm company."))
         return super(DammReportsWizard, self).create(values)
 
     def _search_report_items(self) -> dict[int, object]:
@@ -326,7 +326,7 @@ class DammReportsWizard(models.TransientModel):
             # Avoid weird behaviour with empty data
             # (e.g. missing attachment, 404 Not Found with `/web/content`).
             raise exceptions.UserError(
-                "No items match the given selection, empty report.")
+                _("No items match the given selection, empty report."))
 
         line_data = self._format_report_items(report_items)
         report_name, report_data = self._assemble_report(line_data)
