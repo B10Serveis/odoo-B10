f96c1b4bc8bf3e3a47e25a46f50d89f63f57301e
manolo <software@klo.es>
2025-09-16 10:08:10 +0200

    damm_integration: Paso del módulo a v18.0 desde v15.0 para la exportación de datos a Damm.

--- damm_integration/__manifest__.py
+++ damm_integration/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Integració amb Damm",
     "summary": "Integració de dades amb Damm",
-    "version": "15.0.0.9.0",
+    "version": "18.0.0.9.0",
     "development_status": "Beta",
     "category": "Sales",
     "website": "https://batista10.cat/",
