eb955b042f3b3ebe244dd00f8b139deaf45504b1
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-10 16:57:18 +0200

    b10_sales: Plantilles mostren IBAN de comanda amb diari de pagament.
    (cherry picked from commit fcf4a258d62298664b4bdd3caf411af95b9ca916)

--- b10_sales/__manifest__.py
+++ b10_sales/__manifest__.py
@@ -7,12 +7,15 @@
     "version": "17.0.1.0.0",
     "depends": [
         "sale",
-        "Dissenys_generics",  # account_payment_method_bank_transfer (ref)
+        "Dissenys_generics",  # account_payment_method_bank_transfer (ref), reports
     ],
     "license": "AGPL-3",
     "application": True,
     "data": [
         "views/B10_sale_order_expired_filter.xml",
         "views/sale_order_payment_journal.xml",
+        "report/report_comanda_batista_iban.xml",
+        "report/report_pressupost_batista_iban.xml",
+        "report/report_proforma_batista_iban.xml",
     ],
 }
