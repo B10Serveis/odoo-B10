273b3f9ff0a67dd353797e7e40e33c9342fa8c02
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-14 11:57:57 +0200

    b10_sales: versió 1.1.1
    (cherry picked from commit 87041b3dce5396aac6dabd7422905010616d61c1)

--- b10_sales/__manifest__.py
+++ b10_sales/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.1.1",
     "depends": [
         "sale",
         "Dissenys_generics",  # account_payment_method_bank_transfer (ref), reports
