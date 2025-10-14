178ba7963b81ff11bfdc49c6d66884ed26261911
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-14 15:09:23 +0200

    b10_sales: Fitxers inicials.
    Incloent readme sencer basat en `B10_sales` per a Odoo 17.0.
    
    (cherry picked from commit 3278cc9d1c98922a7ad033f2636efaa28cfc81c7)

---
+++ b10_sales/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "B10 - Sales",
+    "summary": """B10 Sales""",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "category": "Personalization",
+    "version": "15.0.0.0.0",
+    "depends": [
+        "sale",
+    ],
+    "license": "AGPL-3",
+    "application": True,
+    "data": [
+    ],
+}
