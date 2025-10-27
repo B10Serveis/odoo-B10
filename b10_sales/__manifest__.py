9b90512ffafc10b272168fe78921e4c2b71a7d5a
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-27 10:57:09 +0100

    b10_sales: versió 1.5.0, documentació generada
    Amb `odooinstall/build_index.py README.rst`.
    
    (cherry picked from commit c102e35a5ea6572650dffdad6581cc84a2aee813)

--- b10_sales/__manifest__.py
+++ b10_sales/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Batista10 - Sales',
     'summary': 'Personalització Vendes Batista10',
-    'version': '18.0.1.4.1',
+    'version': '18.0.1.5.0',
     'category': 'Sales',
     'author': 'Batista10',
     'website': 'https://www.batista10.cat',
