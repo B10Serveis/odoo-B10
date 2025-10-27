d1e7d466b5ac285b52706e29c4202a86e4818ab5
Batista10-G <gestio@batista10.com>
2025-10-27 16:34:02 +0100

    Mida QR

--- Dissenys_generics/__manifest__.py
+++ Dissenys_generics/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "15.0.1.11.7",
+    "version": "15.0.1.11.8",
     "depends": [
         "base",
         "account_payment_partner",
