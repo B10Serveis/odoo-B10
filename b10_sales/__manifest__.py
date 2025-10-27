7c538fa27eb6710ef516cb871852d6b31636d5d9
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-27 12:47:50 +0100

    [IMP] b10_sales: Inclou mode de pagament a factura si comanda en té.
    (cherry picked from commit a1ea624c851bd223894f22c558fdacad9608b40b)

--- b10_sales/__manifest__.py
+++ b10_sales/__manifest__.py
@@ -7,6 +7,7 @@
     "version": "16.0.1.0.1",
     "depends": [
         "sale",
+        "account_payment_sale",  # account.move:payment_mode_id
         "Dissenys_generics",  # account_payment_method_bank_transfer (ref), reports
     ],
     "license": "AGPL-3",
--- b10_sales/wizard/sale_make_invoice_advance.py
+++ b10_sales/wizard/sale_make_invoice_advance.py
@@ -6,6 +6,8 @@ class SaleAdvancePaymentInv(models.TransientModel):
     # Set invoice bank account if payment journal is set in sale order.
     def _prepare_invoice_values(self, order, so_line):
         invoice_vals = super()._prepare_invoice_values(order, so_line)
+        if payment_mode := order.payment_mode_id:
+            invoice_vals["payment_mode_id"] = payment_mode.id
         if payment_journal := order.payment_journal_id:
             invoice_vals["partner_bank_id"] = payment_journal.bank_account_id.id
         return invoice_vals
