067f82fa751f99018088832acd7482a35e2821f5
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-27 12:14:39 +0100

    B10_account: versió 1.0.1, documentació generada
    Amb `odooinstall/build_index.py README.rst`.

--- B10_account/__manifest__.py
+++ B10_account/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "depends": [
         "account",
         "account_payment_partner",  # account.move:payment_mode_id
