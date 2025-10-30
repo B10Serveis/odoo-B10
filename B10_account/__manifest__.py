e5f4adf86ea770a20006d5176d2413af51b48988
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-30 10:08:25 +0100

    B10_account: versió 1.2.0, documentació generada
    Amb `oca-gen-addon-readme --org-name=B10Serveis --repo-name=odoo-B10
    --branch=16.0 --addon-dir=.`.
    
    (cherry picked from commit b5aaff89de8b5180b41cd868ab26bd71991ee654)

--- B10_account/__manifest__.py
+++ B10_account/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.2.0",
     "depends": [
         "account",
         "account_payment_partner",  # account.move:payment_mode_id
