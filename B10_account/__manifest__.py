8f5e60f4fffc671f5214ab832062a5e06ee7451c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2025-10-30 09:18:00 +0100

    [IMP] B10_account: Considera mode de pagament en computar banc dest.
    El càlcul automàtic anterior es manté quan no el mode no és transferència
    bancària.
    
    (cherry picked from commit f0cbe239b0cb4895b02920240791e17120eb6074)

--- B10_account/__manifest__.py
+++ B10_account/__manifest__.py
@@ -5,7 +5,11 @@
     "website": "https://www.batista10.cat",
     "category": "Personalization",
     "version": "16.0.1.1.0",
-    "depends": ["account", "account_payment_partner",],
+    "depends": [
+        "account",
+        "account_payment_partner",  # account.move:payment_mode_id
+        "Dissenys_generics",  # account_payment_method_bank_transfer (ref)
+    ],
     "license": "AGPL-3",
     "application": True,
     "data": [
