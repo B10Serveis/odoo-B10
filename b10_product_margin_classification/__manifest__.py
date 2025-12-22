25f828435bf8bcd9e38083340970571218f1ac0f
Batista10-G <gestio@batista10.com>
2025-12-22 12:28:06 +0100

    b10_product_margin_classification: creat mòdul i modificació de vista per fer els camps més accessibles

---
+++ b10_product_margin_classification/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "B10 - Product margin classification",
+    "summary": "Modificacions al mòdul product_margin_classification.",
+    "author": "Batista10",
+    "website": "https://www.batista10.cat",
+    "depends": [
+        "base",
+        "product_margin_classification",
+    ],
+    "version": "17.0.1.1.0",
+    "license": "AGPL-3",
+    "application": True,
+    "category": "Personalization",
+    "data": [
+        "views/b10_product_margin.xml",
+    ],
+}
