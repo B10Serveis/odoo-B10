9a6e579c938cbe7a6487f425c01379f39d00c86a
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2026-03-24 10:18:57 +0100

    [IMP] B10_helpdesk_subscriptions: Notes de llicència i autoria.

--- B10_helpdesk_subscriptions/__manifest__.py
+++ B10_helpdesk_subscriptions/__manifest__.py
@@ -1,9 +1,14 @@
+# Copyright (C) 2025 Batista10 (https://www.batista10.cat/).
+# @author: Joan Llimiñana <joan@batista10.cat>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     "name": "Batista10 - Helpdesk Subscriptions",
     "author": ["Batista10"],
     "version": "15.0.1.0.0",
     "application": True,
     "installable": True,
+    "license": "AGPL-3",
     "depends": [
         "base",
         "helpdesk_mgmt",
--- B10_helpdesk_subscriptions/models/ticket_subscription.py
+++ B10_helpdesk_subscriptions/models/ticket_subscription.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2025 Batista10 (https://www.batista10.cat/).
+# @author: Joan Llimiñana <joan@batista10.cat>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import models, fields, exceptions, api, _
 
 
