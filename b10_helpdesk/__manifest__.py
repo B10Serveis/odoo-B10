ce5ff8983447e6125be4a45d2cfd3bf7e68d399b
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2026-03-25 11:37:55 +0100

    [IMP] b10_helpdesk: Sincronitzat amb codi d’altres branques.
    Sense canvis de funcionalitat que justifiquen una nova versió.

--- b10_helpdesk/__manifest__.py
+++ b10_helpdesk/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Batista10",
     "website": "https://www.batista10.cat",
     "category": "Personalization",
-    "version": "18.0.2.1.0",
+    "version": "2.1.0",
     "depends": ["helpdesk_mgmt", "b10_settings_hub"],
     "license": "AGPL-3",
     "application": True,
