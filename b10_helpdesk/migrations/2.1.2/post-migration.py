b63a958baae69c94eab4828003735ec96c0a1410
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2026-04-01 11:14:10 +0200

    [IMP] b10_helpdesk: No habilita contractes si vé de versió recent.
    Ja s’hauria migrat la base de dades, i si no s’ha fet les dades de contractes
    ja estaven perdudes per desgràcia.

--- b10_helpdesk/migrations/2.1.2/post-migration.py
+++ b10_helpdesk/migrations/2.1.2/post-migration.py
@@ -1,11 +1,18 @@
 import logging
 
+from odoo.tools import parse_version
+
+
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     if not version:
         return
-    
+
+    if parse_version(version)[2:] >= parse_version('1.1'):
+        # Aquestes versions ja empren el model de dades nou, res a fer
+        return
+
     # Comprovar que el mòdul nou s’ha instal·lat
     cr.execute("""
         SELECT state FROM ir_module_module
--- b10_helpdesk/migrations/2.1.2/pre-migration.py
+++ b10_helpdesk/migrations/2.1.2/pre-migration.py
@@ -1,3 +1,6 @@
+from odoo.tools import parse_version
+
+
 def migrate(cr, version):
     # Només si s’està actualitzant el mòdul
     if not version:
@@ -12,13 +15,17 @@ def migrate(cr, version):
     if not row:
         return
 
+    if parse_version(version)[2:] >= parse_version('1.1'):
+        # Aquestes versions ja empren el model de dades nou, res a fer
+        return
+
     # Assegurar que el nou mòdul existeix a la llista i marcar-lo per instal·lar
     cr.execute("""
         SELECT id, state FROM ir_module_module
         WHERE name = 'b10_helpdesk_contract'
     """)
     mod = cr.fetchone()
-    if mod:
+    if mod:  # TODO: fallar
         mod_id, state = mod
         if state in ('uninstalled', 'to remove'):
             cr.execute("""
