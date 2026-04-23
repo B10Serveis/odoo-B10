b4e3fcd04ccb0360843bbb937b6992cae082c4f2
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2026-04-23 11:22:39 +0200

    [ADD] b10_base: Nou mòdul de funcions comunes per a mòduls B10.

---
+++ b10_base/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2026 Batista10 (https://www.batista10.cat/).
+# @author: Ivan Vilata i Balaguer <ivan@batista10.cat>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
---
+++ b10_base/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2026 Batista10 (https://www.batista10.cat/).
+# @author: Ivan Vilata i Balaguer <ivan@batista10.cat>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Batista10 - Base",
+    "summary": "Funcions comunes per a mòduls B10",
+    "version": "0.0.0",
+    "development_status": "Alpha",
+    "category": "Customizations",
+    "author": "Batista10",
+    "license": "AGPL-3",
+    "website": "https://www.batista10.cat",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+}
