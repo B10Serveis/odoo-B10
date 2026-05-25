6b1d17e4e35cdfcc65d40617754edb14d1646556
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2026-05-25 12:49:40 +0200

    [IMP] b10_sale_purchase_inter_company: Notes per a incorporació OCA.
    I neutralització de la versió del mòdul.

--- b10_sale_purchase_inter_company/__manifest__.py
+++ b10_sale_purchase_inter_company/__manifest__.py
@@ -1,9 +1,12 @@
 {
     "name": "Batista10 - Inter Company Module for Sale to Purchase Order",
     "summary": "Intercompany SO/PO rules",
-    "version": "17.0.1.0.1",
+    # TODO: Replace with "17.0.X.Y.Z" when part of OCA.
+    "version": "1.0.1",
     "category": "Sales Management",
+    # TODO: Replace with "https://github.com/OCA/multi-company" when part of OCA.
     "website": "https://www.batista10.cat",
+    # TODO: Add ", Odoo Community Association (OCA)" when part of OCA.
     "author": "Batista10",
     "license": "AGPL-3",
     "application": True,
